68aadef0830ef3a43aa8e9caa30d97486fca7408
최재용 <johnchoi213.jc@gmail.com>
2023-05-01 17:21:19 +0900

    Update MemberServiceImpl.java

--- EnjoyTripSpringBoot/src/main/java/com/ssafy/web/service/MemberServiceImpl.java
+++ EnjoyTripSpringBoot/src/main/java/com/ssafy/web/service/MemberServiceImpl.java
@@ -1,62 +1,63 @@
 package com.ssafy.web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
 import com.ssafy.web.dto.MemberDto;
 import com.ssafy.web.repository.MemberRepository;
 import com.ssafy.web.repository.MemberRepositoryImpl;
 
+@Service("MemberServiceImpl")
 public class MemberServiceImpl implements MemberService {
-	private static MemberService memberService = new MemberServiceImpl();
-	private MemberRepository memberDao;
-
-	private MemberServiceImpl() {
-		memberDao = MemberRepositoryImpl.getMemberDao();
-	}
-
-	public static MemberService getMemberService() {
-		return memberService;
-	}
-
+	@Autowired
+	@Qualifier("MemberRepositoryImpl")
+	MemberRepository repo;
+	
 	@Override
 	public int idCheck(String userId) throws Exception {
-		return memberDao.idCheck(userId);
+		return repo.idCheck(userId);
 	}
 
 	@Override
 	public int joinMember(MemberDto memberDto) throws Exception {
 //		memberDto.setUserPwd(BCrypt.hashpw(memberDto.getUserPwd(), BCrypt.gensalt()));
-		memberDto.setUserPwd(memberDto.getUserPwd());
-		System.out.println(memberDto.getUserPwd() + " " + memberDto.getUserPwd().length());
-		return memberDao.joinMember(memberDto);
+		//memberDto.setUserPwd(memberDto.getUserPwd());
+		//System.out.println(memberDto.getUserPwd() + " " + memberDto.getUserPwd().length());
+		//return memberDao.joinMember(memberDto);
+		return repo.joinMember(memberDto);
 	}
 
 	@Override
 	public MemberDto loginMember(String userId, String userPwd) throws Exception {
 		/* userPwd = BCrypt.hashpw(userPwd, BCrypt.gensalt()); */
-		System.out.println(userPwd);
-		MemberDto Member = memberDao.loginMember(userId, userPwd);
+//		System.out.println(userPwd);
+//		MemberDto Member = memberDao.loginMember(userId, userPwd);
+//		
+//		try {
+//			if (userPwd.equals(Member.getUserPwd())) {
+//				return Member;
+//			}
+//			
+//			return (new MemberDto());
+//		} catch (Exception e) {
+//			return null;
+//		}
 		
-		try {
-			if (userPwd.equals(Member.getUserPwd())) {
-				return Member;
-			}
-			
-			return (new MemberDto());
-		} catch (Exception e) {
-			return null;
-		}
-
+		return repo.loginMember(userId, userPwd);
 	}
 
 	@Override
 	public int update(String userId, String userPwd) throws Exception {
 		// TODO Auto-generated method stub
 		/* userPwd = BCrypt.hashpw(userPwd, BCrypt.gensalt()); */
-		return memberDao.update(userId, userPwd);
+		//return memberDao.update(userId, userPwd);
+		return repo.update(userId, userPwd);
 	}
 
 	@Override
 	public int delete(String userId) throws Exception {
 		// TODO Auto-generated method stub
-		return memberDao.delete(userId);
+		return repo.delete(userId);
 	}
 }
