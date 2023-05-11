2744e31c890e1ded3ac6f0b2af158a50ab327c6e
JJewon <eldertree777@gmail.com>
2023-05-12 08:28:46 +0900

    add cors

--- EnjoyTripSpringBoot/src/main/java/com/ssafy/web/EnjoyTripSpringBootApplication.java
+++ EnjoyTripSpringBoot/src/main/java/com/ssafy/web/EnjoyTripSpringBootApplication.java
@@ -2,6 +2,9 @@ package com.ssafy.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EnjoyTripSpringBootApplication {
@@ -9,5 +12,24 @@ public class EnjoyTripSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EnjoyTripSpringBootApplication.class, args);
 	}
+	
+	
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+			    registry.addMapping("/**")
+			      .allowedOrigins("*")
+			      .allowedMethods("GET", "POST", "PUT", "DELETE")
+			      .allowedHeaders("*")
+			      .exposedHeaders("Authorization")
+			      .allowCredentials(false)
+			      .maxAge(3600);
+			  }
+		};
+	}
+	
+	
 
 }
