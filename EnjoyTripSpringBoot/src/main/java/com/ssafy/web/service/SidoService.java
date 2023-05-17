8f1accb9a96800535b8a77e3e6119687e75b2ea5
JJewon <eldertree777@gmail.com>
2023-05-17 11:48:01 +0900

    asd

--- EnjoyTripSpringBoot/src/main/java/com/ssafy/web/service/SidoService.java
+++ EnjoyTripSpringBoot/src/main/java/com/ssafy/web/service/SidoService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ssafy.web.dto.SidoDto;
 import com.ssafy.web.repository.SidoMapper;
@@ -14,6 +15,7 @@ public class SidoService {
 	@Autowired
 	private SidoMapper sidomapper;
 	
+	@Transactional
 	public List<SidoDto> getAllSido(){
 		System.out.println("SidoService");
 		return sidomapper.getAllSido();
